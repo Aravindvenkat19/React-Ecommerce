a2ec5cf082ec077a88ef21e8c652c0c19123f867
Aravindvenkat19 <aravindvenkat1920@gmail.com>
2026-01-14 20:09:22 +0530

    Fix imports and dependencies

--- frontend/latest/src/App.jsx
+++ frontend/latest/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/ProductDetail.jsx";
 import {ToastContainer} from 'react-toastify'
 import Cart from "./pages/Cart.jsx";
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const [cartItems, setCartItems] = useState([]);

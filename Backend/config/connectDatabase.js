4d01cad005bcbb35dd53750dde1ccc706b47de7d
Aravindvenkat19 <aravindvenkat1920@gmail.com>
2026-01-14 12:53:33 +0530

    new Files added successfully

--- Backend/config/connectDatabase.js
+++ Backend/config/connectDatabase.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
-
 const connectDatabase = () => {
-  mongoose.connect(process.env.DB_URL).then((con)=>{
-     console.log("MongoDB Connected to Host:",con.connection.host)
+  if (mongoose.connection.readyState >= 1) {
+    return; // Exit if already connected
+  }
+
+  mongoose.connect(process.env.DB_URL).then((con) => {
+    console.log("MongoDB Connected to Host:", con.connection.host);
+  }).catch((err) => {
+    console.error("MongoDB Connection Error:", err);
   });
 };
 

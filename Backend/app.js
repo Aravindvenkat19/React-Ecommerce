50f47118742d476e9d53f92c43607fb2162d5e53
Aravindvenkat19 <aravindvenkat1920@gmail.com>
2026-01-15 20:45:15 +0530

    updated CORS link

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ connectDatabase();;
 
 app.use(express.json())
 app.use(cors({
-    origin : "https://react-frontend-ecommerce-phi.vercel.app",
+    origin : "https://react-frontend-ecommerce-fzqkqewg4-aravind-venkats-projects.vercel.app",
     credentials: true
 }));
 
